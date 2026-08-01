909171391ff82c5a1140047bbd5f3d951cadbe44
Jagdeep Singh <Jagdeep9540@gmail.com>
2026-08-01 17:26:24 +0530

    Day 26: Added JWT authentication middleware, logout API and token blacklist

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user.models")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
-
+const blacklistModel = require("../models/blacklist.model")
 
 
 async function registerUser(req , res ){
@@ -57,7 +57,8 @@ async function loginUser(req , res){
             { email },
             { username }
         ]
-    })
+    }).select("+password")
+
    if (!user) {
   return res.status(404).json({
     message: "Invalid Credentials"
@@ -95,7 +96,33 @@ return res.status(200).json({
 })
 }
 
+async function getMe(req , res){
+    const user = await userModel.findById(req.user.id)
+
+    res.status(200).json({
+        mesage: "User Fetched Successfully",
+        user
+    }) 
+}
+
+async function logoutUser(req, res){
+
+    const token = req.cookies.token;
+
+    
+    res.clearCookie("token")
+
+    await blacklistModel.create({
+        token
+    })
+    res.status(200).json({
+        message: "User Logged out Successfully"
+    })
+}
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getMe,
+    logoutUser
 }
