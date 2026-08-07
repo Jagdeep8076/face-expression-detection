d4a72f5c9e9ec60cf3552f64f127f9a1da74c6a5
Jagdeep Singh <Jagdeep9540@gmail.com>
2026-08-07 15:39:01 +0530

    Fix public path

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,11 +18,11 @@ const songRoutes = require("./routes/song.routes")
 app.use("/api/auth", authRoutes)
 app.use("/api/songs", songRoutes)
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
+})
 
 module.exports = app;
 
