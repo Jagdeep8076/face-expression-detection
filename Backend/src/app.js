166b03540a3d3543b87fa95100a9c0442100c355
Jagdeep Singh <Jagdeep9540@gmail.com>
2026-08-08 12:18:37 +0530

    fix: auth initialization and production cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,28 +1,36 @@
 const path = require("path");
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-   origin: "http://localhost:5173",
-   credentials: true
-}))
+const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
 
-const authRoutes = require("./routes/auth.routes")
-const songRoutes = require("./routes/song.routes")
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://face-expression-detection-10a1.onrender.com"
+        ],
+        credentials: true
+    })
+);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/songs", songRoutes)
+const authRoutes = require("./routes/auth.routes");
+const songRoutes = require("./routes/song.routes");
 
+app.use("/api/auth", authRoutes);
+app.use("/api/songs", songRoutes);
+
+// Frontend build
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../public", "index.html"));
-})
+    res.sendFile(
+        path.join(__dirname, "../public", "index.html")
+    );
+});
 
 module.exports = app;
-
